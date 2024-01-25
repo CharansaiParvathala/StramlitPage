fcd6d06424f74802324ae58466eaadaab999f79b
CharansaiParvathala <154997286+CharansaiParvathala@users.noreply.github.com>
2024-01-25 15:56:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -189,7 +189,7 @@ if page == 'Feedback':
         st.markdown('\n\n\n\n**Please share your suggestions to help Me to improve the webpage:**')
         with st.form('Form1'):
              subm = st.text_area('Submit your suggestion')
-             if st.form_submit_button('Submit')
+             if st.form_submit_button('Submit'):
                 if subm = "":
                    st.warning("Please Fill the Text Field")
                 else:
@@ -198,7 +198,7 @@ if page == 'Feedback':
         st.markdown('\n\n\n\n**Please share your suggestions to help Me to improve the webpage:**')
         with st.form('Form2'):
              sub = st.text_area('Submit your suggestion')
-             if st.form_submit_button('Submit')
+             if st.form_submit_button('Submit'):
                 if sub = "":
                    st.warning('Please Fill the Text Field')
                 else:
