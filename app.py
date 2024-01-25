22ce332bfe28ab04217564879545d0bd38c59bc7
CharansaiParvathala <154997286+CharansaiParvathala@users.noreply.github.com>
2024-01-25 12:26:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -93,8 +93,8 @@ if page == 'Home':
 
     if rad == 'SQL':
         st.markdown("**SQL Basic** Certification From **Hackerrank**\n\n"
-        "HackerRank is well known platform to everyone\n"
-        "Learning SQL is very challenging and exciting")
+        "HackerRank is well known platform to everyone.\n"
+        "Learning SQL in HackerRank is very challenging and exciting")
         
         st.link_button('See SQL Certificate', 'https://www.hackerrank.com/certificates/0b7851c7519e')
 
